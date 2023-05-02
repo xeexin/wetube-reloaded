d90186213b3bc1e19a15210023d1c262d7b3b4bd
HEEJIN LIM <127469548+xeexin@users.noreply.github.com>
2023-05-02 18:43:41 +0900

    3.2 Get Request

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import express from "express";
 const app = express(); 
 const PORT = 4000;
 
+app.get("/", () => console.log("Somebody is trying to go home"));
+
 const handleListening = () => console.log(`✅ Server Listening on port http://localhost:${PORT} 👻`);
 app.listen(4000, handleListening);
 
