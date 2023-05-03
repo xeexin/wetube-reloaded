6b9618d480bfcefccb2a04364eac64eb2c006c60
HEEJIN LIM <127469548+xeexin@users.noreply.github.com>
2023-05-03 20:58:15 +0900

    3.5 MiddleWare part 1

--- src/server.js
+++ src/server.js
@@ -4,18 +4,21 @@ import express from "express";
 const app = express(); 
 const PORT = 4000;
 
+const gossipMiddleware = (req, res, next) => {
+    console.log(`Someone is going to: ${req.url}`);
+    next();
+}
 
 const handleHome = (request, response) => {
-    return response.send("<h1>I still love you</h1>");
- 
-    //return response.end();
+    //return response.send("<h1>I still love you</h1>");
+    return response.send("I love Middlewares");
 };
 
 const handleLogin = (req, res) => {
     return res.send ({message: "Login here"});
 }
 
-app.get("/", handleHome);
+app.get("/", gossipMiddleware, handleHome);
 app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`✅ Server Listening on port http://localhost:${PORT} 👻`);
