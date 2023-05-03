cf2460629e64e4a058980dd9137b7ab837f21407
HEEJIN LIM <127469548+xeexin@users.noreply.github.com>
2023-05-03 21:16:13 +0900

    3.6 Middleware part 2
    .use

--- src/server.js
+++ src/server.js
@@ -4,24 +4,35 @@ import express from "express";
 const app = express(); 
 const PORT = 4000;
 
-const gossipMiddleware = (req, res, next) => {
-    console.log(`Someone is going to: ${req.url}`);
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
     next();
 }
+
+const privateMiddleware = (req, res, next) => {
+    const url = req.url ; 
+    if( url === "/protected" ){
+        return res.send("<h1>Not Allowed</h1>");
+    };
+    console.log("Allowed, you may continue");
+    next();
+};
 
 const handleHome = (request, response) => {
     //return response.send("<h1>I still love you</h1>");
     return response.send("I love Middlewares");
 };
 
-const handleLogin = (req, res) => {
-    return res.send ({message: "Login here"});
+const handleProtected = (req, res) => {
+    return res.sned("Welcome to the private lounge")
 }
 
-app.get("/", gossipMiddleware, handleHome);
-app.get("/login", handleLogin);
+app.use(logger);
+app.use(privateMiddleware);
+
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
+
 
 const handleListening = () => console.log(`✅ Server Listening on port http://localhost:${PORT} 👻`);
 app.listen(4000, handleListening);
-
-// =app.listen(4000, () => console.log("Server Listening on port 4000👻");
