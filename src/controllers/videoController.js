62343771ac22d04683fb52c6c87ab2f7123f41ef
HEEJIN LIM <127469548+xeexin@users.noreply.github.com>
2023-05-06 02:17:48 +0900

    3.7 URL parameters part 1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,9 @@
 
 export const trending = (req,res) => res.send("Home page videos");
 
-export const see = (req, res) => res.send("Watch video");
+export const see = (req, res) => {
+    return res.send(`Watch video #${req.params.id}`);
+};
 
 export const edit = (req,res) => res.send("Edit viedo");
 
@@ -9,4 +11,6 @@ export const search = (req, res) => res.send("Search Video");
 
 export const upload = (req, res) => res.send("Upload video");
 
-export const deleteVideo = (req,res) => res.send("Delete Video");
+export const deleteVideo = (req,res) => {
+    return res.send("Delete Video");
+} ;
