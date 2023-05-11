3e55451dbff76063e4a989de8247998802396265
HEEJIN LIM <127469548+xeexin@users.noreply.github.com>
2023-05-12 00:27:28 +0900

    5.9 Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,14 +2,29 @@
 export const trending = (req, res) => {
     const videos = [
         {
-            title: "HELLO"
-        },
-        {
-            title: "Video 2"
-        },
-        {
-            title:"Whatsup"
-        }
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+          },
+          {
+            title: "Second Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+          },
+          {
+            title: "Third Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+          },
     ];
 
     return res.render("home", {pageTitle: "Home", videos});
