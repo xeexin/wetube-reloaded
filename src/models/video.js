0316eb17014235dc90b9bb14406efd46f46a5715
HEEJIN LIM <127469548+xeexin@users.noreply.github.com>
2023-05-16 00:32:58 +0900

    6.17 Execptions and Validation

--- src/models/video.js
+++ src/models/video.js
@@ -2,15 +2,15 @@ import e from "express";
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    title : String,
-    description : String,
-    createdAt : Date,
-    hashtags : [{type:String}],
-    mdta : {
-        views: Number,
-        rating: Number,
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    createdAt: { type: Date, required: true, default: Date.now },
+    hashtags: [{ type: String }],
+    meta: {
+      views: { type: Number, default: 0, required: true },
+      rating: { type: Number, default: 0, required: true },
     },
-});
+  });
 
 const Video = mongoose.model("Video", videoSchema);
 
