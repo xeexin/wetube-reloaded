1aca1193b3ef2b1bb4654e5c48e213fd96c92174
HEEJIN LIM <127469548+xeexin@users.noreply.github.com>
2023-05-15 22:00:03 +0900

    6.12 Our First Query part2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,16 +1,22 @@
-import Video from "../models/video";
+import Video from "../models/video.js";
 
 export const home = (req, res) => {
-
-  Video.find({}, (error, videos) => {});
-  return res.render("home", {pageTitle: "Home"});
+  console.log('Start');
+  Video.find({})
+    .exec()
+    .then((videos) => {
+      console.log('Finished');
+      res.render('home', { pageTitle: 'Home', videos });
+    })
+    .catch((error) => {});
+  console.log('I finish first');
 };
 
 export const watch = (req, res) => {
   const { id } = req.params;
   res.render("watch",{ pageTitle: `Watching`});
 
-} 
+} ;
 
 export const getEdit = (req,res) => {
   const { id } = req.params;
@@ -22,14 +28,14 @@ export const postEdit = (req, res) => {
   const { title } = req.body;
   return res.redirect(`/videos/${id}`);
 
-}
+};
 
 export const getUpload = (req, res) => {
   return res.render("upload", {pageTitle: "Upload Video"});
-}
+};
 
 export const postUpload = (req, res) => {
   const { title } = req.body;
   return res.redirect("/");
 
-}
+};
