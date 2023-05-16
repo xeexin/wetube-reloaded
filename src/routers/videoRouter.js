62d3888068115b5c1720a1ab43046a2d81dc6ff8
HEEJIN LIM <127469548+xeexin@users.noreply.github.com>
2023-05-17 01:03:20 +0900

    6.25 Detete Video

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,11 @@
 import  express from "express";
-import { watch, getEdit, postEdit, getUpload, postUpload } from "../controllers/videoController";
+import { watch, getEdit, postEdit, getUpload, postUpload, deleteVideo } from "../controllers/videoController";
 
 const videoRouther = express.Router();
 
 videoRouther.get("/:id([0-9a-f]{24})", watch);
 videoRouther.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouther.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouther.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouther;
