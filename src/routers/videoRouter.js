cfe71790f5bb84de2ee145a538887e55c3e8b913
HEEJIN LIM <127469548+xeexin@users.noreply.github.com>
2023-05-16 21:06:27 +0900

    6.19 Video Detail
    16진수
    findbyid

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,8 +3,8 @@ import { watch, getEdit, postEdit, getUpload, postUpload } from "../controllers/
 
 const videoRouther = express.Router();
 
-videoRouther.get("/:id(\\d+)", watch);
-videoRouther.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouther.get("/:id([0-9a-f]{24})", watch);
+videoRouther.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouther.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouther;
