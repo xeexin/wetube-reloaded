4c815d5e643d49cf25d7deb3f5125a17bf25355e
HEEJIN LIM <127469548+xeexin@users.noreply.github.com>
2023-05-16 21:36:35 +0900

    6.21 Edit Video part2
    startsWith()

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -23,15 +23,25 @@ export const getEdit = async (req,res) => {
   return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.findById(id);
+  if(!video){
+    return res.render("404", {pageTitle: "Video not found."});
+  }
+  video.title =title;
+  video.description = description;
+  video.hashtags = hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`));
+  await video.save();
   return res.redirect(`/videos/${id}`);
 
 };
 
 export const getUpload = (req, res) => {
+
   return res.render("upload", {pageTitle: "Upload Video"});
+
 };
 
 export const postUpload = async (req, res) => {
@@ -40,7 +50,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      hashtags: hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`)),    
     });
     return res.redirect("/");
   } catch (error) {
