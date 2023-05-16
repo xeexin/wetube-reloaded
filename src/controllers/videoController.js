f66fa3ec7764425cd77348a8a5a6b4ecce4d1517
HEEJIN LIM <127469548+xeexin@users.noreply.github.com>
2023-05-17 01:13:21 +0900

    6.26 Search part 1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 import Video from "../models/video.js";
 
 export const home = async(req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", {pageTitle: "Home", videos})
 };
 
@@ -67,4 +67,12 @@ export const deleteVideo = async (req, res) => {
   const { id } = req.params;
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
+};
+
+export const search = (req, res) => {
+  const { keyword } = req.query;
+  if (keyword) {
+    // search
+  }
+  return res.render("search", { pageTitle: "Search" });
 };
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -9,5 +9,5 @@ const globalRouter = express.Router();
 globalRouter.get("/", home);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
-
+globalRouter.get("/search", search);
 export default globalRouter;
